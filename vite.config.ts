db9165d1517b9af3f0bf6338fe388712bcb4891c
LofiTea <iwilldiscoveraurora@gmail.com>
2025-02-07 16:55:15 -0500

    Fixed a bug

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/BejaranoNotes/",
+  base: "/cs251/",
   build: {
     outDir: "docs",
   },
